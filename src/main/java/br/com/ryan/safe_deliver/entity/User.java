655adbbd6064622be0fb8f94bc1eb43b30fc3b20
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-29 02:37:28 -0300

    classe user implementa user details

--- src/main/java/br/com/ryan/safe_deliver/entity/User.java
+++ src/main/java/br/com/ryan/safe_deliver/entity/User.java
@@ -7,7 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -29,4 +34,42 @@ public class User {
     @JoinColumn(name = "hospital_id")
     @JsonBackReference
     private Hospital hospital;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role.equals(Role.HOSPITAL_MANAGER)) return List.of(new SimpleGrantedAuthority("ROLE_HOSPITAL_MANAGER"));
+        if (this.role.equals(Role.DRIVER)) return List.of(new SimpleGrantedAuthority("ROLE_DRIVER"));
+        if (this.role.equals(Role.SYS_ADMIN)) return List.of(new SimpleGrantedAuthority("ROLE_SYS_ADMIN"));
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

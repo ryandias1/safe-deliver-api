4da3684b83d7c8ea0081bbd5ac5abdc350e1cb4a
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-29 01:31:18 -0300

    commit inicial

---
+++ src/main/java/br/com/ryan/safe_deliver/SafeDeliverApplication.java
@@ -0,0 +1,13 @@
+package br.com.ryan.safe_deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeDeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ryan/safe_deliver/SafeDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ryan.safe_deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
